c5af28faed729faf9c1ed2e8b24ebbbc725e6975
ssahu9 <sumit.ps94@gmail.com>
2019-07-14 19:31:14 +0530

    First Component

---
+++ myapp/src/Ninjas.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class Ninjas extends Component {
+
+render(){
+    return(
+        <div className="ninjas">
+            <p>Name: Sumit</p>
+            <p>Age: 25</p>
+            
+        </div>
+    )
+}
+
+}
+
+export default Ninjas
