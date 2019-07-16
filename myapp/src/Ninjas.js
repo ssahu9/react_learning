6ed170a2243813d72c5089b6189d868bba014080
ssahu9 <sumit.ps94@gmail.com>
2019-07-16 23:04:47 +0530

    used conditioal operator

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 
 // class Ninjas extends Component {
@@ -6,6 +6,7 @@ import React, {Component} from 'react';
     const Ninjas= ({ninjas})=>{
     // const {ninjas} = props
     const ninjaList = ninjas.map( ninja =>{
+        if(ninja.age>25){
         return(
              <div className="ninjas" key={ninja.id}>
             <p>Name: {ninja.name}</p>
@@ -13,6 +14,22 @@ import React, {Component} from 'react';
             <p>Belt: {ninja.belt}</p>
         </div>     
         )
+    }
+    else 
+    return null
+    })
+
+    const ninjaList2 = ninjas.map( ninja =>{
+        return (ninja.age>25) ? (
+            <div className="ninjas" key={ninja.id}>
+            <p>Name: {ninja.name}</p>
+            <p>Age: {ninja.age}</p>
+            <p>Belt: {ninja.belt}</p>
+        </div>
+     ) : null
+        
+    
+    
     })
     return(
         <div className="ninjas">
