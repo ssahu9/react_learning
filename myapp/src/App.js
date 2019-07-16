994ebc567ebac32e1412465d6f39320aec4deee8
ssahu9 <sumit.ps94@gmail.com>
2019-07-16 23:42:22 +0530

    function as a props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,6 +1,7 @@
 import React ,{Component} from 'react';
 import logo from './logo.svg';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja'
 
 
 
@@ -13,14 +14,22 @@ class App extends Component {
       { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
+  addNinja = (ninja) =>{
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas: ninjas
+    })
+  }
 
-  
  render(){
   return (
     <div className="App">
       <h1>My first React app</h1>
 
       <Ninjas ninjas={this.state.ninjas} />
+      <AddNinja addNinja ={this.addNinja}/>
+
     </div>
   );
 }
